fb0a077243d767c1969b4a28c5b6a9cacd7997f8
w-j-gannon <wgannon@austin.utexas.edu>
2019-03-17 19:17:21 -0500

    transaction()

--- customer.js
+++ customer.js
@@ -23,7 +23,7 @@ var connection = mysql.createConnection ({
 // check connection, start readStock;
 connection.connect(function(err) {
     if (err) throw err;
-    console.log("Connected to the Bamazon Server\n");
+    console.log("Welcome to the Bamazon!\n");
     readStock();
 });
 
@@ -41,7 +41,78 @@ function readStock() {
 
 // prompt customer for sale, go through transaction, end connection after
 function transaction() {
-    
+    inquirer
+        prompt([
+            {
+                type: "input",
+                name: "item",
+                message: "Enter the ID of the item you would like to purchase.",
+                validate: function(value) {
+                    if (isNaN(value) == false) {
+                        return true
+                    } else {
+                        return false;
+                    }
+                }
+            }, {
+                type: "input",
+                name: "quantity",
+                message: "Enter quantity.",
+                validate: function(value) {
+                    if (isNaN(value) == false) {
+                        return true
+                    } else {
+                        return false;
+                    }
+                }
+            }
+        ])
+        .then(function(sale) {
+            var orderQuantity = parseInt(sale.quantity);
+            var orderId = sale.item;
+            // pick item and select data
+            connection.query(
+                "SELECT stock_quantity, price FROM products WHERE ?", [{
+                    item_id: orderId
+                }], function(err, res) {
+                    if (err) throw (err);
+                    // if enough stock, set new amount & run updateStock. Log sale amount
+                    var price = res[0].price;
+                    if (orderQuantity <= res[0].stock_quantity) {
+                        var newQuantity = res[0].stock_quantity - orderQuantity;
+                        var salePrice = orderQuantity * price;
+                        updateStock(newQuantity, orderId);
+                        console.log("Sale complete. You will be charged $ " + salePrice + ".")
+
+                        // ask for another transaction
+                        inquirer
+                            prompt([
+                                {
+                                    type: "list",
+                                    name: "resale",
+                                    message: "Would you like to make another purchase?",
+                                    choices: ["yes", "no"]
+                                }
+                            ]).then(function(answer) {
+
+                                // yes, rerun list and prompts
+                                if (answer.resale === "yes") {
+                                    readStock();
+
+                                // no, goodbye and end connection
+                                } else {
+                                    console.log("Thank you for visiting Bamazon." );
+                                    connection.end();
+                                }
+                            })
+                    // if not enough stock, try again
+                    } else {
+                        console.log("Sorry, low stock. Try a lower quantity or select another item.");
+                        transaction();
+                    }
+                }
+            )
+        })
 };
 
 // update database after sale
